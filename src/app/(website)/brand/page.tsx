095695ca38d26f4da0509abab1a95759cf4d0a9c
Tom Elliott <tomelliottnz@gmail.com>
2025-04-20 12:18:51 +1200

    branding

--- src/app/(website)/brand/page.tsx
+++ src/app/(website)/brand/page.tsx
@@ -1,16 +1,31 @@
 export default function Page() {
   return (
     <div className="flex justify-center ">
-      <div className="container p-4 flex flex-col gap-8">
+      <div className="container px-4 py-8 flex flex-col gap-8">
         <div className="border-b">
-          <h1 className="text-xl">iNZight Analytics Brand</h1>
+          <h1 className="text-4xl mb-4">iNZight Analytics Brand</h1>
         </div>
 
-        <h2 className="text-lg">Colours</h2>
-        <div className="h-32 flex gap-4 bg-gray-300 p-4">
-          <div className="w-3/5 bg-black"></div>
-          <div className="w-3/10 bg-white"></div>
-          <div className="w-1/10 bg-red-600"></div>
+        <div className="">
+          <h2 className="text-2xl mb-2">Colours</h2>
+          <div className="h-32 flex gap-4 bg-gray-300 p-4">
+            <div className="w-3/5 bg-black"></div>
+            <div className="w-3/10 bg-white"></div>
+            <div className="w-1/10 bg-red-600"></div>
+          </div>
+        </div>
+
+        <div>
+          <h2 className="text-2xl mb-2">Typography</h2>
+
+          <div className="bg-gray-300 p-4">
+            <h1 className="text-4xl mb-4">Heading level 1</h1>
+            <h2 className="text-2xl mb-2">Heading level 2</h2>
+            <h3 className="text-xl mb-1">Heading level 3</h3>
+            <h4 className="font-bold mb-1">Heading level 4</h4>
+
+            <p>Normal paragraph text.</p>
+          </div>
         </div>
       </div>
     </div>
