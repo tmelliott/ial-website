1c898ecfd03a573039a1943fb2f77ab47968976f
Tom Elliott <tomelliottnz@gmail.com>
2025-08-10 09:23:12 +1200

    undo self-hosting config

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,18 @@ import withPayload from "@payloadcms/next/withPayload";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone",
+  // output: "standalone", // for self-hosting
   images: {
     remotePatterns: [
-      new URL("http://localhost:3000/**"),
-      new URL("https://inzight.co.nz/**"),
+      {
+        protocol: "https",
+        hostname: "inzight.co.nz",
+      },
+      // new URL("http://localhost:3000/**"),
+      // new URL("https://inzight.co.nz/**"),
     ],
   },
-  compress: false,
+  // compress: false,
 };
 
 export default withPayload(nextConfig);
