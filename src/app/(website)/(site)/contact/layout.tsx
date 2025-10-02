d107c25461be2cb6137199dd7fadcd3911c6c972
Tom Elliott <tomelliottnz@gmail.com>
2025-10-02 13:45:30 +1300

    col in small

--- src/app/(website)/(site)/contact/layout.tsx
+++ src/app/(website)/(site)/contact/layout.tsx
@@ -6,7 +6,7 @@ export default async function Layout({
   return (
     <div className="">
       <div className="bg-linear-200 from-15% from-[var(--color-bg-gradient-start)] to-[150%] to-[var(--color-bg-gradient-end)]  p-8 text-white shadow-sm pb-12 lg:pb-24">
-        <div className="max-w-6xl flex justify-between gap-8 mx-auto pt-12 lg:pt-36">
+        <div className="max-w-6xl flex flex-col lg: justify-between gap-8 mx-auto pt-12 lg:pt-36">
           <header>
             <h1 className="text-6xl font-display pb-12 flex flex-col gap-4">
               <div>Whakapā Mai</div>
